d8ea590fbaaf7cfe3a6d660f016a4be582af0ee6
Alina Gomeniuc <agomeniuc@gmail.com>
2020-02-22 23:00:27 +0200

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Alina Gomeniuc on 2/22/20.
+//  Copyright © 2020 Alina Gomeniuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
